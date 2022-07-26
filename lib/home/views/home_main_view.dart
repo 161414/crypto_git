6e5789bdecf41ab529d4130d8c6f0627c146de79
shamna parveen <shamna@techfriar.com>
2022-07-26 12:12:35 +0530

    login added

---
+++ lib/home/views/home_main_view.dart
@@ -0,0 +1,56 @@
+import 'package:authentication_with_bloc/authenticaiton/bloc/authentication_bloc.dart';
+import 'package:authentication_with_bloc/login/views/login_main_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final user= FirebaseAuth.instance.currentUser;
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+          actions: [
+            Row(
+              children: [
+                Text('${user?.displayName}'),
+                IconButton(
+                  icon: Icon(
+                    Icons.exit_to_app,
+                  ),
+                  onPressed: () => BlocProvider.of<AuthenticationBloc>(context).add(
+                    AuthenticationExited(),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        body: Center(
+          child: BlocConsumer<AuthenticationBloc, AuthenticationState>(
+            listener: (context, state) {
+              if (state is AuthenticationFailiure) {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (_) => LoginMainView()));
+              }
+            },
+            builder: (context, state) {
+              if (state is AuthenticationInitial) {
+                BlocProvider.of<AuthenticationBloc>(context)
+                    .add(AuthenticationStarted());
+                return CircularProgressIndicator();
+              } else if (state is AuthenticationLoading) {
+                return CircularProgressIndicator();
+              } else if (state is AuthenticationSuccess) {
+                return Text('Welcome :${state.authenticationDetail.uid}');
+              }
+              return Text('Undefined state : ${state.runtimeType}');
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
